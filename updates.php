99299e68aa71538513bdadcc2b1eeeb2a1deb5bb
Kyle Olive <kolive@uwaterloo.ca>
2014-04-28 08:14:50 -0400

    rdio last-song-played functionality

---
+++ updates.php
@@ -0,0 +1,39 @@
+<?php
+    require_once 'includes/rdio.php';
+    
+    
+    $reqtype = htmlspecialchars($_GET["type"]);
+    
+    if($reqtype === "rdio"){        		
+        $url = 'http://api.rdio.com/1/';
+        $fields = array(
+                        'vanityName' => 'alunatic',
+                        'extras' => 'lastSongPlayed,lastSongPlayTime'
+                );
+        $rdio = new Rdio(array("RDIOKEY", "RDIOKEY"));
+        $result = $rdio->call("findUser", $fields);
+        $result = get_object_vars($result->result->lastSongPlayed);
+        
+        $lastAlbumCover = $result['icon'];
+        $lastAlbum = $result['album'];
+        $lastArtist = $result['albumArtist'];
+        $lastName = $result['name'];
+        
+        $urlArtist = urlencode($lastartist);
+        $urlAlbum = urlencode($lastalbum);
+        
+        $artistUrl = "http://www.amazon.ca/gp/search?search-alias=popular&field-artist=$urlArtist&field-title=$urlAlbum";
+        $albumUrl = "http://en.wikipedia.org/wiki/$urlArtist";
+        
+        $resultsArray = array( songName => $lastName, albumName => $lastAlbum, albumCover => $lastAlbumCover, artist => $lastArtist, artistUrl => $artistUrl, albumUrl => $albumUrl);
+        echo json_encode($resultsArray);
+    }else if($reqtype === "goodreads"){
+        echo "goodreads";
+    }else if($reqtype === "twitter"){
+        echo "twitter";
+    }else if($reqtype === "github"){
+        echo "github";
+    }
+
+
+?>
